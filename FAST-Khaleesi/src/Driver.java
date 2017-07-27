1aa7d4e8867c4b2042c4e97ed77fc987b01be092
Shihabur Rahman Chowdhury <sr2chowdhury@uwaterloo.ca>
2017-07-27 09:49:33 -0400

    Misc.

--- FAST-Khaleesi/src/Driver.java
+++ FAST-Khaleesi/src/Driver.java
@@ -66,8 +66,8 @@ public class Driver {
 		linkSelectionWriter.close();
 		durationWriter.close();
 		
-		// int startFlowIndex = 0, endFlowIndex = flowsList.size() - 1;
-		int startFlowIndex = 176, endFlowIndex = 176;
+		int startFlowIndex = 0, endFlowIndex = flowsList.size() - 1;
+		// int startFlowIndex = 176, endFlowIndex = 176;
 		for (int flowIdx = startFlowIndex; flowIdx <= endFlowIndex; ++flowIdx) {
 			ArrayList<Tuple> vLinks = generateE(flowsList.get(flowIdx), rcm);
 			System.out.println("List of Es \n" + vLinks);
